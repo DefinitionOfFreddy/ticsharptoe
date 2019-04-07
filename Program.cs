ebabdbc0c9a0eaa535d4b088ff877e1698aaefa6
Penel Frédéric <frederic.penel@gmx.fr>
2019-04-07 09:17:58 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TicSharpToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+A little Tic-tac-toe game for practicing C# while following the "Bug Free by design" principle.
+
+See the following BugsZero-Kata repository https://github.com/martinsson/BugsZero-Kata
