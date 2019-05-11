b368d9394895516acab900e46fcbfdfa9c8cb1b5
Penel Frédéric <frederic.penel@gmx.fr>
2019-05-11 21:12:05 +0200

    Init Board

--- Game.cs
+++ Game.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace TicSharpToe
 {
@@ -11,16 +12,44 @@ namespace TicSharpToe
             Console.WriteLine("Game initialized");
             Console.WriteLine("Player 1 symbol : " + this.playerOne.Symbol.ToString());
             Console.WriteLine("Player 2 symbol : " + this.playerTwo.Symbol.ToString());
-            
+            board = new Dictionary<Position, Symbol>();
+            board[Position.NORTHWEST] = Symbol.None;
+            board[Position.NORTH] = Symbol.None;
+            board[Position.NORTHEST] = Symbol.None;
+            board[Position.WEST] = Symbol.None;
+            board[Position.CENTER] = Symbol.None;
+            board[Position.EST] = Symbol.None;
+            board[Position.SOUTHWEST] = Symbol.None;
+            board[Position.SOUTH] = Symbol.None;
+            board[Position.SOUTHEST] = Symbol.None;
         }
 
         public void Play()
         {
             Console.WriteLine("Starting game");
+            Console.WriteLine("Board Status :");
+            Console.WriteLine(this.BoardToString());
             Console.WriteLine("Ending game");
         }
 
+        private string BoardToString()
+        {
+            return "| "  + board[Position.NORTHWEST].ToString() +
+                            " | " + board[Position.NORTH].ToString() +
+                            " | " + board[Position.NORTHEST].ToString() +
+                            " |\n" +
+                            "| " + board[Position.WEST].ToString() +
+                            " | " + board[Position.CENTER].ToString() +
+                            " | " + board[Position.EST].ToString() +
+                            " |\n" +
+                            "| " + board[Position.SOUTHWEST].ToString() +
+                            " | " + board[Position.SOUTH].ToString() +
+                            " | " + board[Position.SOUTHEST].ToString() +
+                            " |";
+        }
+        
         private Player playerOne;
         private Player playerTwo;
+        private Dictionary<Position, Symbol> board;
     }
 }
---
+++ Position.cs
@@ -0,0 +1,15 @@
+namespace TicSharpToe
+{
+    public enum Position
+    {
+        NORTHWEST,
+        NORTH,
+        NORTHEST,
+        WEST,
+        CENTER,
+        EST,
+        SOUTHWEST,
+        SOUTH,
+        SOUTHEST
+    }
+}
